0a0eeb46380a6ba7831537e53e6ff1fcf9e5f76c
jasher33 <jasher33@gmail.com>
2016-07-24 12:10:56 -0700

    asf

--- HelloExpress.js
+++ HelloExpress.js
@@ -47,18 +47,6 @@ app.post('/post-loopback', function(req,res){
   res.render('post-loopback', context);
 });
 
-app.use(function(req,res){
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
-});
-
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.send('500 - Server Error');
-});
 app.get('/',function(req,res){
     var context = {};
   //If there is no session, go to the main page.
@@ -112,6 +100,20 @@ app.post('/',function(req,res){
   context.toDo = req.session.toDo;
   console.log(context.toDo);
   res.render('toDo',context);
+});
+
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
 });
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
