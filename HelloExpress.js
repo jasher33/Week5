aa50ad32fcbc02d9eef795667c54b828c13f4c6d
jasher33 <jasher33@gmail.com>
2016-07-24 12:09:12 -0700

    asdfsadfasdf

--- HelloExpress.js
+++ HelloExpress.js
@@ -11,20 +11,6 @@ var session = require('express-session');
 
 app.use(session({secret:'SuperSecretPassword'}));
 
-
-
-app.get('/',function(req,res){
-  res.render('home.handlebars') //We can omit the .handlebars extension as we do below
-});
-
-app.get('/other-page',function(req,res){
-  res.render('other-page');
-});
-
-app.get('/math', function(req, res){
-	res.render('math', genContext());
-});
-
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -73,7 +59,60 @@ app.use(function(err, req, res, next){
   res.status(500);
   res.send('500 - Server Error');
 });
+app.get('/',function(req,res){
+    var context = {};
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length || 0;
+  context.toDo = req.session.toDo || [];
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
 
+app.get('/other-page',function(req,res){
+  res.render('other-page');
+});
+
+app.get('/math', function(req, res){
+	res.render('math', genContext());
+});
+
+app.post('/',function(req,res){
+  var context = {};
+
+  if(req.body['New List']){
+    req.session.name = req.body.name;
+    req.session.toDo = [];
+    req.session.curId = 0;
+  }
+
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+
+  if(req.body['Add Item']){
+    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+    req.session.curId++;
+  }
+
+  if(req.body['Done']){
+    req.session.toDo = req.session.toDo.filter(function(e){
+      return e.id != req.body.id;
+    })
+  }
+
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length;
+  context.toDo = req.session.toDo;
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
