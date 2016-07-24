1d60c8176e22d6a7ca956f590f15150eaf89e70c
jasher33 <jasher33@gmail.com>
2016-07-24 11:28:44 -0700

    coutn changw

--- HelloExpress.js
+++ HelloExpress.js
@@ -7,6 +7,17 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
+var session = require('express-session');
+
+app.use(session({secret:'SuperSecretPassword'}));
+
+app.get('/count',function(req,res){
+  var context = {};
+  context.count = req.session.count || 0;
+  req.session.count = context.count + 1;
+  res.render('count', context);
+});
+
 app.get('/',function(req,res){
   res.render('home.handlebars') //We can omit the .handlebars extension as we do below
 });
@@ -17,6 +28,23 @@ app.get('/other-page',function(req,res){
 
 app.get('/math', function(req, res){
 	res.render('math', genContext());
+});
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/post-loopback', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('post-loopback', context);
 });
 
 app.use(function(req,res){
